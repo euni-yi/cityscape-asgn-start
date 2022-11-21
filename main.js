f67bfa0d1c16842e374aa25caccb1f474889a874
euni-yi <113129884+euni-yi@users.noreply.github.com>
2022-11-21 11:23:05 -0700

    left building fin

--- main.js
+++ main.js
@@ -31,3 +31,20 @@ ctx.fillRect(150, 100, 150, 550);
 ctx.fillRect(170, 40, 110, 60);
 // Small Top
 ctx.fillRect(190, 20, 70, 20);
+
+// square windows on left building
+for (let x = 40; x <= 115; x = x + 23) {
+  for (let y = 210; y <= 345; y = y + 23){
+    ctx.fillStyle = "white";
+    ctx.fillRect(x, y, 11, 11);
+  }
+}
+
+// horizontal windows on left building
+ for(let x=20; x<=70; x=x+20){
+     for(let y=400; y<=600; y=y+50){
+     ctx.fillStyle = "white";
+     ctx.fillRect(x, y, 50, 20);
+     }
+ }
+
